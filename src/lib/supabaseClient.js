5f875ff3dbba38a73b8f3a0e20c662f12a2f21ee
Pedro D'Anello <pedrogomesdanello1@gmail.com>
2025-09-10 23:04:39 -0300

    Configura variáveis de ambiente para deploy

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://seiebvksmpugjvlmzjsc.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlaWVidmtzbXB1Z2p2bG16anNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1MzU2NjQsImV4cCI6MjA3MzExMTY2NH0.fdDyYgPkLDUoDAQDlFq6X2jLhjQzL_RXQ32ttx9CjLw'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
