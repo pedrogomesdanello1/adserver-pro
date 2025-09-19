e4ebef4408b32aba0bc2cf0a8a3766c31c1cc78f
Pedro D'Anello <pedrogomesdanello1@gmail.com>
2025-09-18 21:27:49 -0300

    style: ajustar design elegante dos ícones nos cards de status com corte no canto e opacity 35%

--- src/components/dashboard/StatusCards.jsx
+++ src/components/dashboard/StatusCards.jsx
@@ -116,9 +116,9 @@ const StatusCards = ({ demandas = [], onStatusClick, selectedStatus }) => {
               <Icon className={`w-5 h-5 ${config.color}`} />
             </div>
             
-            {/* Ícone de fundo com transparência */}
-            <div className="absolute top-4 right-4 opacity-50">
-              <Icon className={`w-16 h-16 ${config.color}`} />
+            {/* Ícone de fundo com transparência - design elegante cortando parte da imagem */}
+            <div className="absolute -top-2 -right-2 opacity-35 overflow-hidden">
+              <Icon className={`w-20 h-20 ${config.color}`} />
             </div>
             
             <div className="flex items-baseline">
